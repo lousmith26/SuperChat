9dd50916d71a94db7b02149a7da706921748562b
Louis Smith <smith.louis245@gmail.com>
2022-01-11 02:04:34 -0500

    created main components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,19 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
-      <form method="POST" action="/signup" id="form">
-        <input type="text" id="first_name" name="first_name" />
-        <input type="text" id="last_name" name="last_name" />
-        <button type="submit">Send</button>
-      </form>
-    </div>
-     
-    <Routes>
-      <Route path="/" element={ user ? <Main user={ user }/> : <Signup /> } />
-      <Route path="/signup" element={ <Signup /> } />
-      <Route path="/signin" element={ <Signin /> } />
-    </Routes>
-
+      <div className="App">
+        <form method="POST" action="/signup" id="form">
+          <input type="text" id="first_name" name="first_name" />
+          <input type="text" id="last_name" name="last_name" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+      
+      <Routes>
+        <Route path="/" element={ user ? <Main user={ user }/> : <Signup /> } />
+        <Route path="/signup" element={ <Signup /> } />
+        <Route path="/signin" element={ <Signin /> } />
+      </Routes>
     </BrowserRouter>
   );
 }
